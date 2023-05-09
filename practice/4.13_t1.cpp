d05ead86d2c5c1db408c4c86d575a232cab2f55c
XSSD <117296065+XSSD169124@users.noreply.github.com>
2023-05-09 23:07:57 +0800

    Add files via upload

---
+++ practice/4.13_t1.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+int main()
+{
+	using namespace std;
+	char first_name[20];
+	char last_name[20];
+	char grade;
+	int age;
+	cout << "What's your first name?"<<endl;
+	cin.getline(first_name,20);
+	cout << "What's your last name"<<endl;
+	cin.getline(last_name,20);
+	cout << "What letter grade do you deserve?"<<endl;
+	cin>>grade;
+	grade++;
+	cout << "What's your age?"<<'/n';
+	cin >> age;
+	cout << "Name:" << last_name << "," << first_name << endl;
+	cout << "Grade:";
+	cout.put(grade);
+	cout << endl<<"Age:" << age;
+	return 0;
+
+}
---
+++ practice/4.13_t2.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<string>
+int main()
+{
+	using namespace std;
+	string name;
+	string dessert;
+	cout << "Enter your name: " ;
+	getline(cin,name);
+	cout << "Enter your favorite dessert: ";
+	getline(cin, dessert);
+	cout << "i have some delicious " << dessert;
+	cout << " for you," << name << "." << endl;
+	return 0;
+
+}
